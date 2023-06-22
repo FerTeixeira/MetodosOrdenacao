3ab691daf09d3c23f66c8983878bf4690d57d889
FerTeixeira <120655231+FerTeixeira@users.noreply.github.com>
2023-06-21 21:53:56 -0300

    Adicionando recursividade

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         //Criação do primeiro array
-        int[] vetor = {3,6,8,1,4,9,0};
+        int[] vetor = {3, 6, 8, 1, 4, 9, 0};
         //Criação de um objeto da classe Sort
         Sort a = new Sort();
         //Usando o método SelectionSort para organizar o array
-        vetor = a.SelectionSort(vetor);
+        a.SelectionSort(vetor);
         System.out.println("Selection Sort");
-        for(int i = 0; i < vetor.length; i++)
-        {
-            System.out.print(vetor[i]  + " ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
         }
-        //Criação do segundo array
-        int[] vetor2 = {3,6,8,1,4,9,0};
+
+        int[] vetor2 = {3, 6, 8, 1, 4, 9, 0};
         //Usando o método BubbleSort para organizar o array
-        vetor2 = a.BubbleSort(vetor2);
+        a.BubbleSort(vetor2, vetor2.length);
         System.out.println();
         System.out.println("Bubble Sort");
-        for(int i = 0; i < vetor2.length; i++)
-        {
-            System.out.print(vetor2[i]  + " ");
+        for (int i = 0; i < vetor2.length; i++) {
+            System.out.print(vetor2[i] + " ");
+
         }
 
-
     }
+
 }
--- src/Sort.java
+++ src/Sort.java
@@ -2,8 +2,7 @@ public class Sort {
 
     public int[] SelectionSort(int[] dados)
     {
-        //Variável para armazenar a posição de um número durante a permutação
-        int aux;
+
         //Variável para armazenar a posição do menor número do array
         int menor;
         //Loop para percorrer o array 2 vezes
@@ -22,53 +21,52 @@ public class Sort {
                 }
             }
             //Permutação para mover os número dentro do array
-            aux = dados[i];
-            dados[i] = dados[menor];
-            dados[menor] = aux;
+            Permutacao(dados, i, menor);
+
         }
         return dados;
 
     }
-    public int[] BubbleSort(int[] dados)
+    //Método para fazer permutacao
+    public void Permutacao(int[] dados, int i, int j)
     {
-        //Variável para não permitir que o loop faça comparação do último item com o próximo(que é inexistente)
-        int coluna = 1;
-        //Variável para armazenar a posição de um número durante a permutação
-        int aux;
-        //Loop para percorrer o array 2 vezes
-        for(int i = 0; i < dados.length - 1; i++)
+        int aux = dados[i];
+        dados[i] = dados[j];
+        dados[j] = aux;
+    }
+    public void BubbleSort(int[] dados, int n)
+    {
+        //Variável para contar a quantidade de vezes que a permuta não acontece
+        int naopermuta = 0;
+
+        //Loop para percorrer o array
+        for(int i = 0; i < n - 1; i++) {
+
+            //Comparação da posição[i] com a posição[i + 1]
+            if (dados[i] > dados[i + 1]) {
+
+                //Permutação para mover os número dentro do array
+                Permutacao(dados, i, i + 1);
+
+            } else {
+                //Contador para a quantidade de vezes que não permuta
+                naopermuta++;
+            }
+
+        }
+        //Se a quandade de vezes que não aconteceu permutação for igual ao tamanho do array - 1 o array já está ordenado
+        // e o loop é encerrado antes de repetir todas as vezes.
+        if (naopermuta == dados.length -1)
         {
-            //Variável para contar a quantidade de vezes que a permuta não acontece
-            int naopermuta = 0;
-            for (int j = 0; j < dados.length - coluna; j++)
-            {
-                //Comparação da posição[j] com a posição[j + 1]
-                if (dados[j] > dados[j + 1])
-                {
-                    //Permutação para mover os número dentro do array
-                    aux = dados[j];
-                    dados[j] = dados[j + 1];
-                    dados[j + 1] = aux;
-                }
-                else
-                {
-
-                    naopermuta++;
-                }
-
-            }
-            coluna++;
-            //Se a quandade de vezes que não aconteceu permutação for igual ao tamanho do array - 1 o array já está ordenado
-            // e o loop é encerrado antes de repetir todas as vezes.
-            if (dados.length - 1 == naopermuta)
-            {
-                return dados;
-
-            }
+            return;
+        }
+        //Condição de saída da recursividade
+        if (n -1 > 1){
+            BubbleSort( dados, n - 1);
         }
 
-        return dados;
-    }
 
+    }
 
 }
+
