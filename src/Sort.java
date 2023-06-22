dbb1e8b0bf83d12756ba247172b0fa6252a0ce9f
FerTeixeira <120655231+FerTeixeira@users.noreply.github.com>
2023-06-22 14:12:26 -0300

    Arummando Comentários

--- src/Sort.java
+++ src/Sort.java
@@ -1,4 +1,11 @@
 public class Sort {
+    //Método para fazer permutacao
+    public void Permutacao(int[] dados, int i, int j)
+    {
+        int aux = dados[i];
+        dados[i] = dados[j];
+        dados[j] = aux;
+    }
 
     public int[] SelectionSort(int[] dados)
     {
@@ -15,11 +22,16 @@ public class Sort {
                 //Comparação para saber em qual posição está o menor número ([j] é o segundo loop, [menor] é o primeiro loop)
                 if(dados[j] < dados[menor])
                 {
-                    //Se o número da posição [j] for menor que o número da posição [menor] armazena-se a variável [j] na variável [menor]
+                    //Se o número da posição [j] for menor que o número da posição [menor] armazena-se a posição [j] na variável [menor]
+
                     menor = j;
 
                 }
             }
+            //Depois do primeiro loop comparar seu valor com todos os valores do segundo loop
+            //é feita a permutação com a posição que estava armazenada na posição da variável menor, trocando a posição do menor valor com o analisado pelo primeiro loop
+
+
             //Permutação para mover os número dentro do array
             Permutacao(dados, i, menor);
 
@@ -27,13 +39,7 @@ public class Sort {
         return dados;
 
     }
-    //Método para fazer permutacao
-    public void Permutacao(int[] dados, int i, int j)
-    {
-        int aux = dados[i];
-        dados[i] = dados[j];
-        dados[j] = aux;
-    }
+
     public void BubbleSort(int[] dados, int n)
     {
         //Variável para contar a quantidade de vezes que a permuta não acontece
@@ -54,16 +60,20 @@ public class Sort {
             }
 
         }
-        //Se a quandade de vezes que não aconteceu permutação for igual ao tamanho do array - 1 o array já está ordenado
+        //Se a quantidade de vezes que não aconteceu permutação for igual ao tamanho do array - 1 o array já está ordenado
         // e o loop é encerrado antes de repetir todas as vezes.
         if (naopermuta == dados.length -1)
         {
             return;
         }
         //Condição de saída da recursividade
-        if (n -1 > 1){
+        //Se o array ainda não chegou no fim o método BubbleSorte faz o loop mais uma vez
+        //diminuindo o tamanho da lista
+        else if (n -1 > 1){
             BubbleSort( dados, n - 1);
         }
+
+        //Quando chega no tamanho da lista - 1 o método finaliza
 
 
     }
